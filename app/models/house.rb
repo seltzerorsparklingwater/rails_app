853f162dfc244aa04f8e606e14aa8aa1e4beb1ca
emily <emily.m.westfall@gmail.com>
2019-11-20 10:12:29 -0500

    create associations for house and residents/person

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,4 +1,9 @@
 class House < ApplicationRecord
     validates :address, presence: true
 
+    has_many :residents,
+        primary_key: :id, #house's id
+        foreign_key: :house_id,
+        class_name: :Person
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,9 @@ class Person < ApplicationRecord
     validates :name, presence: true
     validates :house_id, presence: true
 
+    belongs_to :house,
+        primary_key: :id, #house's id 
+        foreign_key: :house_id,
+        class_name: :House
+
 end
